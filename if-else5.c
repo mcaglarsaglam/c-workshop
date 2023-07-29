aa6fff600f1460eeba936062a377c59632051e39
mcaglarsaglam <muhammedcaglara@gmail.com>
2023-07-29 19:39:51 +0300

    add: if-else5.c

---
+++ if-else5.c
@@ -0,0 +1,35 @@
+
+#include <stack>
+main()
+{
+
+  // iç içe if:
+
+  // Take 2 numbers from the user and print them on the screen according to upper and lower case letters.
+
+  int number1;
+  int number2;
+
+  printf("Please enter 2 numbers\n");
+  scanf("%d %d", &number1, &number2);
+
+  if (number1 >= number2)
+  {
+
+    if (number1 == number2)
+    {
+      printf("CONCLUSION:%d=%d", number1, number2);
+    }
+
+    else
+    {
+
+      printf("CONCLUSION:%d>%d", number1, number2);
+    }
+  }
+  else
+  {
+    printf("CONCLUSION:%d<%d", number1, number2);
+  }
+  return 0;
+}
