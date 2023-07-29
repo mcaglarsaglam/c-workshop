18d042a44beb78f56feba2552b1cb3237b2f839c
mcaglarsaglam <muhammedcaglara@gmail.com>
2023-07-29 19:47:00 +0300

    add: for2.c

---
+++ for2.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+main()
+{
+
+  // Print the odd and even numbers from 0 to 10 with a for loop
+
+  int i;
+
+  for (i = 0; i <= 10; i += 2)
+  {
+    printf("%d\n", i);
+  }
+
+  int a;
+
+  for (a = 1; a <= 10; a += 2)
+  {
+    printf("%d\n", a);
+  }
+
+  return 0;
+}
