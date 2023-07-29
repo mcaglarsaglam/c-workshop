c3ceb849831b4edc929a0a17b80778da58e37d3c
mcaglarsaglam <muhammedcaglara@gmail.com>
2023-07-30 01:59:54 +0300

    add: casting

---
+++ casting/casting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+
+    // casting
+    // int sayı =2.25
+    // printf("%d",sayı);
+
+    float number = (float)9 / 4;
+
+    printf("%f", number);
+
+    return 0;
+}
