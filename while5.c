110c05ee0e00dce1c6ea3c715645e0b9adaa9596
mcaglarsaglam <muhammedcaglara@gmail.com>
2023-07-29 19:55:58 +0300

    add: while5.c

---
+++ while5.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+main()
+{
+
+  // Press the numbers starting from 10 and decreasing by 1 to the screen.
+
+  int i = 10;
+
+  while (i >= 1)
+  {
+    printf("%d\n", i);
+    i--;
+  }
+  return 0;
+}
