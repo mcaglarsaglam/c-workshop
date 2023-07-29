6d599e6aab6f08cd1cc44515af3b0cb55ae7ef15
mcaglarsaglam <muhammedcaglara@gmail.com>
2023-07-30 01:19:45 +0300

    add: ascı

---
+++ ascı/ascı1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+mein()
+{
+
+  // ASCI
+
+  printf("%d\t %d\t %d\t\n", 'a', 'b', 'c');
+
+  int i;
+
+  for (i = 'a'; i <= 'z'; i++)
+  {
+    printf("%c\n", i);
+  }
+
+  return 0;
+}
