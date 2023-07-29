c944fe9645417ffc958110c945eba6b66f0cdfbf
mcaglarsaglam <muhammedcaglara@gmail.com>
2023-07-30 01:48:09 +0300

    add: math.h

---
+++ math.h/math.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+
+main()
+{
+
+    // ceil floor sqrt pow abs
+
+    printf("%f\n", ceil(4.2));
+    printf("%f\n", floor(4.9));
+    printf("%f\n", sqrt(45));
+    printf("%f\n", pow(3, 4));
+    printf("%d\n", abs(15));
+
+    return 0;
+}
