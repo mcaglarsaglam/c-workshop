8221dd5e88bc5fb250e93e989088011d2388d6fd
mcaglarsaglam <muhammedcaglara@gmail.com>
2023-08-04 13:25:56 +0300

    add: character-series9.c

---
+++ character-series/strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  // Strlen
+
+  char caglar[] = "July";
+
+  printf("The size of our character array = %d", strlen(caglar));
+
+  return 0;
+}
